e099d9dde864fa1e92b9fa087522fd1005401e03
奶油话梅糖 <1097681347@qq.com>
2022-03-03 08:40:40 +0800

    upload

---
+++ app/src/main/java/com/etang/mt_launcher/launcher/settings/desktopsetting/DeskTopSettingList.java
@@ -0,0 +1,16 @@
+package com.etang.mt_launcher.launcher.settings.desktopsetting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.etang.mt_launcher.R;
+
+public class DeskTopSettingList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setting_activity_desk_top_list);
+    }
+}
