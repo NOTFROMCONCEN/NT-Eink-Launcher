fd40ae32d37314c8f920427b88e2fddb14832698
Huameitang <xiaoyu13561171914@163.com>
2024-04-24 11:03:40 +0800

    upload
    Signed-off-by: Huameitang <xiaoyu13561171914@163.com>

--- app/src/main/java/com/etang/mt_launcher/launcher/settings/about/AboutActivity.java
+++ app/src/main/java/com/etang/mt_launcher/launcher/settings/about/AboutActivity.java
@@ -36,7 +36,7 @@ import com.etang.mt_launcher.tool.mtcore.MTCore;
 public class AboutActivity extends AppCompatActivity {
     private ImageView iv_about_logo;//关于 LOGO
     //文本，分别是文本_返回，文本_标题，文本_按钮，文本_关于捐赠我
-    private TextView tv_title, tv_button, tv_about_juanzeng, tv_about_showversion, tv_about_guanzhu, tv_about_showsysinfo;
+    private TextView tv_title, tv_button, tv_about_juanzeng, tv_about_showversion, tv_about_showsysinfo;
     //返回LinearLayout
     private LinearLayout lv_back;
     //检查更新按钮
@@ -113,16 +113,6 @@ public class AboutActivity extends AppCompatActivity {
                 PayMeDialog.show_dialog(AboutActivity.this);
             }
         });
-        //弹出关注方式Dialog
-        tv_about_guanzhu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Toast提示
-                MTCore.showToast_new("使用对应APP扫码即可~~~~", true);
-                //显示捐赠弹出框
-                MTCore.FollwoMeDialog(AboutActivity.this);
-            }
-        });
         //设置版本号
 //        tv_about_showversion.setText("梅糖桌面 Project" + "\n" + MTCore.get_my_appVERSIONNAME());
 //        tv_about_showversion.setText("梅糖桌面 Project" + "\n" + BuildConfig.VERSION_NAME);
@@ -152,7 +142,6 @@ public class AboutActivity extends AppCompatActivity {
         tv_title = (TextView) findViewById(R.id.tv_title_text);
         btn_about_checkup_button = (Button) findViewById(R.id.btn_about_checkup_button);
         tv_about_juanzeng = (TextView) findViewById(R.id.tv_about_juanzeng);
-        tv_about_guanzhu = (TextView) findViewById(R.id.tv_about_guanzhu);
         tv_about_showsysinfo = (TextView) findViewById(R.id.tv_about_showsysinfo);
     }
 
