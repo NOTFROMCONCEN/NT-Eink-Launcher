0b938d4573929a2c37bc327bcbdd4c951b71caea
Huameitang <1097681347@qq.com>
2025-01-26 11:16:39 +0800

    upload

--- app/src/main/java/com/etang/mt_launcher/tool/getapps/DeskTopGridViewBaseAdapter.java
+++ app/src/main/java/com/etang/mt_launcher/tool/getapps/DeskTopGridViewBaseAdapter.java
@@ -2,6 +2,7 @@ package com.etang.mt_launcher.tool.getapps;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Build;
 import android.view.LayoutInflater;
@@ -14,6 +15,7 @@ import android.widget.TextView;
 
 import androidx.annotation.RequiresApi;
 
+import com.bumptech.glide.Glide;
 import com.etang.mt_launcher.R;
 import com.etang.mt_launcher.tool.beans.Bean_AppInfo;
 import com.etang.mt_launcher.tool.mtcore.savearrayutil.SaveArrayImageUtil;
@@ -22,22 +24,20 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * @Package: com.etang.nt_launcher.tool.util
- * @ClassName: DeskTopGridViewBaseAdapter
- * @Description: java类作用描述
- * @CreateDate: 2021/3/19 8:27
- * @UpdateDate: 2021/3/19 8:27
- */
 public class DeskTopGridViewBaseAdapter extends BaseAdapter {
-    Context context;
-    List<Bean_AppInfo> beanAppInfos = new ArrayList<Bean_AppInfo>();
-    //当前页面TAG
-    private static String TAG = "DeskTopGridViewBaseAdapter";
+    private static final String TAG = "DeskTopGridViewBaseAdapter";
+    private Context context;
+    private List<Bean_AppInfo> beanAppInfos = new ArrayList<>();
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences appListStatePrefs;
+    private ArrayList<String> searchArrayList;
 
     public DeskTopGridViewBaseAdapter(List<Bean_AppInfo> beanAppInfos, Context context) {
         this.beanAppInfos = beanAppInfos;
         this.context = context;
+        this.sharedPreferences = context.getSharedPreferences("info", context.MODE_PRIVATE);
+        this.appListStatePrefs = context.getSharedPreferences("info_app_list_state", context.MODE_PRIVATE);
+        this.searchArrayList = SaveArrayImageUtil.getSearchArrayList(context);
     }
 
     @Override
@@ -60,107 +60,88 @@ public class DeskTopGridViewBaseAdapter extends BaseAdapter {
     public View getView(final int position, View convertView, ViewGroup parent) {
         Holder holder = null;
         if (convertView == null) {
-            convertView = LayoutInflater.from(context).inflate(
-                    R.layout.list_gridview_item, null);
+            convertView = LayoutInflater.from(context).inflate(R.layout.list_gridview_item, null);
             holder = new Holder();
-            holder.ico = (ImageView) convertView.findViewById(R.id.iv_gridview_app_icon);
-            holder.Name = (TextView) convertView.findViewById(R.id.tv_gridview_app_name);
-            holder.line_appinfo = (LinearLayout) convertView.findViewById(R.id.line_appinfo);
+            holder.ico = convertView.findViewById(R.id.iv_gridview_app_icon);
+            holder.Name = convertView.findViewById(R.id.tv_gridview_app_name);
+            holder.line_appinfo = convertView.findViewById(R.id.line_appinfo);
             convertView.setTag(holder);
         } else {
             holder = (Holder) convertView.getTag();
         }
-        holder.ico.setImageBitmap(beanAppInfos.get(position).getIco());
-        get_iconsize(context, holder);
-        holder.Name.setText(beanAppInfos.get(position).getName());
-        ArrayList<String> arrayList = new ArrayList<>();
-        arrayList = SaveArrayImageUtil.getSearchArrayList(context);
-        for (int i = 1; i < arrayList.size(); i++) {
-            String str = arrayList.get(i);
-            String[] all = str.split("-");
-            if (beanAppInfos.get(position).getPackageName().equals(all[0])) {
-                holder.ico.setImageURI(Uri.fromFile(new File("sdcard/ntlauncher/" + all[1])));
+
+        Bean_AppInfo appInfo = beanAppInfos.get(position);
+        holder.ico.setImageBitmap(appInfo.getIco());
+        getIconSize(holder);
+        holder.Name.setText(appInfo.getName());
+
+        for (int i = 1; i < searchArrayList.size(); i++) {
+            String[] all = searchArrayList.get(i).split("-");
+            if (appInfo.getPackageName().equals(all[0])) {
+                Glide.with(context).load(new File(Environment.getExternalStorageDirectory(), "ntlauncher/" + all[1])).into(holder.ico);
+                break;
             }
         }
-        get_appname_info(holder);//读取APP名称和边框大小设置
+
+        getAppNameInfo(holder);
+
         return convertView;
     }
 
-
-    private void get_iconsize(Context context, Holder holder) {
-        try {
-            SharedPreferences sharedPreferences = context.getSharedPreferences("info", context.MODE_PRIVATE);
-            int size = Integer.valueOf(sharedPreferences.getString("icon_size", null));
-            ViewGroup.LayoutParams params = holder.ico.getLayoutParams();
-            params.height = size;
-            params.width = size;
-            holder.ico.setLayoutParams(params);
-        } catch (Exception e) {
-            SharedPreferences sharedPreferences = context.getSharedPreferences("info", context.MODE_PRIVATE);
-            sharedPreferences.edit().putString("icon_size", "45").commit();
-            get_iconsize(context, holder);
-        }
+    private void getIconSize(Holder holder) {
+        int size = sharedPreferences.getInt("icon_size", 45);
+        ViewGroup.LayoutParams params = holder.ico.getLayoutParams();
+        params.height = size;
+        params.width = size;
+        holder.ico.setLayoutParams(params);
     }
 
-    /**
-     * 读取APP名称和边框大小设置
-     *
-     * @param holder
-     */
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
-    private void get_appname_info(Holder holder) {
-        SharedPreferences sharedPreferences;
-        sharedPreferences = context.getSharedPreferences("info_app_list_state", context.MODE_PRIVATE);
-        String appname_state = sharedPreferences.getString("appname_state", null);
-        String appblok_state = sharedPreferences.getString("appblok_state", null);
-        try {
-            if (sharedPreferences != null) {
-                if (appname_state.equals("one")) {
-                    holder.Name.setSingleLine(true);
-                    holder.Name.setVisibility(View.VISIBLE);
-                }
-                if (appname_state.equals("nope")) {
-                    holder.Name.setSingleLine(false);
-                    holder.Name.setVisibility(View.VISIBLE);
-                }
-                if (appname_state.equals("hind")) {
-                    holder.Name.setVisibility(View.GONE);
-                }
-                if (appname_state.equals("null")) {
-                    holder.Name.setSingleLine(true);
-                    holder.Name.setVisibility(View.VISIBLE);
-                }
-                if (appblok_state.equals("null")) {
-                    holder.line_appinfo.setBackground(null);
-                }
-                if (appblok_state.equals("hind_blok")) {
-                    holder.line_appinfo.setBackground(null);
-                }
-                if (appblok_state.equals("show_blok_line")) {
-                    holder.line_appinfo.setBackgroundResource(R.drawable.shaper_zhijiao_lins_show);
-                }
-                if (appblok_state.equals("show_blok_yuan")) {
-                    holder.line_appinfo.setBackgroundResource(R.drawable.shaper_yuanjiao_lins_show);
-                }
-                if (appblok_state.equals("show_nocolor_blok_line")) {
-                    holder.line_appinfo.setBackgroundResource(R.drawable.shaper_zhijiao_lins_nocolor);
-                }
-                if (appblok_state.equals("show_nocolor_blok_yuan")) {
-                    holder.line_appinfo.setBackgroundResource(R.drawable.shaper_yuanjiao_nocolor_show);
-                }
-                if (appblok_state.equals("show_blok")) {
-                    holder.line_appinfo.setBackgroundResource(R.drawable.shaper_yuanjiao_lins_show);
-                }
-                if (appblok_state.equals("show_nocolor_blok")) {
-                    holder.line_appinfo.setBackgroundResource(R.drawable.shaper_yuanjiao_nocolor_show);
-                }
-            }
-        } catch (Exception e) {
-            SharedPreferences.Editor editor = context.getSharedPreferences("info_app_list_state", context.MODE_PRIVATE).edit();
-            editor.putString("appname_state", "one");
-            editor.putString("appblok_state", "show_nocolor_blok_yuan");
-            editor.apply();
-            get_appname_info(holder);
+    private void getAppNameInfo(Holder holder) {
+        String appnameState = appListStatePrefs.getString("appname_state", "one");
+        String appblokState = appListStatePrefs.getString("appblok_state", "show_nocolor_blok_yuan");
+
+        switch (appnameState) {
+            case "one":
+                holder.Name.setSingleLine(true);
+                holder.Name.setVisibility(View.VISIBLE);
+                break;
+            case "nope":
+                holder.Name.setSingleLine(false);
+                holder.Name.setVisibility(View.VISIBLE);
+                break;
+            case "hind":
+                holder.Name.setVisibility(View.GONE);
+                break;
+            case "null":
+                holder.Name.setSingleLine(true);
+                holder.Name.setVisibility(View.VISIBLE);
+                break;
+        }
+
+        switch (appblokState) {
+            case "null":
+            case "hind_blok":
+                holder.line_appinfo.setBackground(null);
+                break;
+            case "show_blok_line":
+                holder.line_appinfo.setBackgroundResource(R.drawable.shaper_zhijiao_lins_show);
+                break;
+            case "show_blok_yuan":
+                holder.line_appinfo.setBackgroundResource(R.drawable.shaper_yuanjiao_lins_show);
+                break;
+            case "show_nocolor_blok_line":
+                holder.line_appinfo.setBackgroundResource(R.drawable.shaper_zhijiao_lins_nocolor);
+                break;
+            case "show_nocolor_blok_yuan":
+                holder.line_appinfo.setBackgroundResource(R.drawable.shaper_yuanjiao_nocolor_show);
+                break;
+            case "show_blok":
+                holder.line_appinfo.setBackgroundResource(R.drawable.shaper_yuanjiao_lins_show);
+                break;
+            case "show_nocolor_blok":
+                holder.line_appinfo.setBackgroundResource(R.drawable.shaper_yuanjiao_nocolor_show);
+                break;
         }
     }
 
