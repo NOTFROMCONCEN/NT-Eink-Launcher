e3fc79fdf9e06ee3ad995d995574a1f62a2f499e
Huameitang <1097681347@qq.com>
2025-02-05 15:24:01 +0800

    2025年2月5日
    Signed-off-by: Huameitang <1097681347@qq.com>

--- app/src/main/java/com/etang/mt_launcher/tool/getapps/DeskTopGridViewBaseAdapter.java
+++ app/src/main/java/com/etang/mt_launcher/tool/getapps/DeskTopGridViewBaseAdapter.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
